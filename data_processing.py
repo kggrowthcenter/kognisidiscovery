2a7f10de5f9e7ef9ac5cb16325bd64f8abf4d892
kggrowthcenter <data@growthcenter.id>
2025-07-02 01:34:09 +0700

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -2,24 +2,24 @@ import streamlit as st
 import pandas as pd
 from fetch_data import fetch_data_discovery_al, fetch_data_discovery_au, fetch_data_sap, fetch_data_capture, fetch_data_creds
 
-@st.cache_data
+@st.cache_data(ttl=1800)
 def fill_empty_with_na(df, value='N/A'):
     """Fill empty or None values in the DataFrame with the specified value."""
     return df.replace(['', None, '#VALUE!'], value).fillna(value)
 
-@st.cache_data
+@st.cache_data(ttl=1800)
 def fetch_discovery_al_data():
     df_discovery_al = fetch_data_discovery_al()
     df_discovery_al['email'] = df_discovery_al['email'].str.strip().str.lower()
     return fill_empty_with_na(df_discovery_al)
 
-@st.cache_data
+@st.cache_data(ttl=1800)
 def fetch_discovery_au_data():
     df_discovery_au = fetch_data_discovery_au()
     df_discovery_au['email'] = df_discovery_au['email'].str.strip().str.lower()
     return fill_empty_with_na(df_discovery_au)
 
-@st.cache_data
+@st.cache_data(ttl=1800)
 def fetch_sap_data():
     selected_columns = ['name_sap', 'email', 'nik', 'unit', 'subunit', 'admin_hr', 'layer', 'generation', 'gender', 'division', 'department', 'tenure']
     df_sap = fetch_data_sap(selected_columns)
@@ -27,7 +27,7 @@ def fetch_sap_data():
     df_sap['nik'] = df_sap['nik'].astype(str).str.zfill(6)
     return fill_empty_with_na(df_sap)
 
-@st.cache_data
+@st.cache_data(ttl=1800)
 def fetch_capture_data():
     df_capture_sheet1, df_capture_sheet2 = fetch_data_capture()
     df_capture_sheet1['email'] = df_capture_sheet1['email'].str.strip().str.lower()
@@ -36,7 +36,7 @@ def fetch_capture_data():
     df_capture_sheet2 = fill_empty_with_na(df_capture_sheet2)
     return df_capture_sheet1, df_capture_sheet2
 
-@st.cache_data
+@st.cache_data(ttl=1800)
 def finalize_data():
     df_discovery_al = fetch_discovery_al_data()
     df_discovery_au = fetch_discovery_au_data()
